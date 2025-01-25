02dcc6f5024688a84dd13f09dc8f4912d01324e3
eiredrake <eiredrake@gmail.com>
2025-01-24 23:27:48 -0500

    moved attributes into a table.

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -8,16 +8,12 @@ TRENCHCRUSADE.abilities = {
   mov: 'TRENCHCRUSADE.Ability.Mov.long',
   mel: 'TRENCHCRUSADE.Ability.Mel.long',
   ran: 'TRENCHCRUSADE.Ability.Ran.long',
-  arm: 'TRENCHCRUSADE.Ability.Arm.long',
-  wis: 'TRENCHCRUSADE.Ability.Wis.long',
-  cha: 'TRENCHCRUSADE.Ability.Cha.long',
+  arm: 'TRENCHCRUSADE.Ability.Arm.long'
 };
 
 TRENCHCRUSADE.abilityAbbreviations = {
   mov: 'TRENCHCRUSADE.Ability.Mov.abbr',
   mel: 'TRENCHCRUSADE.Ability.Mel.abbr',
   ran: 'TRENCHCRUSADE.Ability.Ran.abbr',
-  arm: 'TRENCHCRUSADE.Ability.Arm.abbr',
-  wis: 'TRENCHCRUSADE.Ability.Wis.abbr',
-  cha: 'TRENCHCRUSADE.Ability.Cha.abbr',
+  arm: 'TRENCHCRUSADE.Ability.Arm.abbr'
 };
