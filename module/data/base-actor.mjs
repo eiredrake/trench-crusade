181eb9cabadede1c0076a7a590d66cf8252a6e9d
eiredrake <eiredrake@gmail.com>
2025-01-24 23:57:25 -0500

    added costs to unit.

--- module/data/base-actor.mjs
+++ module/data/base-actor.mjs
@@ -16,6 +16,11 @@ export default class TrenchCrusadeActorBase extends TrenchCrusadeDataModel {
     });
     schema.biography = new fields.StringField({ required: true, blank: true }); // equivalent to passing ({initial: ""}) for StringFields
 
+    schema.cost = new fields.SchemaField({
+      ducats: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0}),
+      glory: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0}),
+    });
+
     return schema;
   }
 
