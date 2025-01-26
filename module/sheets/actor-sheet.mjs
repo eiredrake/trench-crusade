90382474dd49df8938ad6ef316dee5f3bf9e9a21
eiredrake <eiredrake@gmail.com>
2025-01-26 00:12:07 -0500

    broken - keywords do not save to actor

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -45,8 +45,10 @@ export class TrenchCrusadeActorSheet extends ActorSheet {
     const actorData = this.document.toPlainObject();
 
     // Add the actor's data to context.data for easier access, as well as flags.
-    context.system = actorData.system;
-    context.flags = actorData.flags;
+    // context.system = actorData.system;
+    // context.flags = actorData.flags;
+    context.system = this.actor.system;
+    context.flags = this.actor.flags;
 
     // Adding a pointer to CONFIG.TRENCHCRUSADE
     context.config = CONFIG.TRENCHCRUSADE;
